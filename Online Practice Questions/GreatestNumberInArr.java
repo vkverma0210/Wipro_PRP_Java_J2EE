1a72ec426c8b0c1a52b5f3372a99de845a27f082
VKVerma0210 <vkverma0210@gmail.com>
2021-07-30 18:55:20 +0530

    ScannerClass Change

--- Online Practice Questions/GreatestNumberInArr.java
+++ Online Practice Questions/GreatestNumberInArr.java
@@ -1,27 +1,29 @@
 import java.util.Scanner;
 
 public class GreatestNumberInArr {
+    private static Scanner scn;
+
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
+        scn = new Scanner(System.in);
 
         int n = scn.nextInt();
 
         int[] arr = new int[n];
 
-        for(int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             arr[i] = scn.nextInt();
         }
 
         int ans = Integer.MIN_VALUE;
 
-        for(int i = 0 ; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             if (arr[i] > ans) {
                 ans = arr[i];
             }
         }
-        
+
         System.out.println(ans);
+
+        scn.close();
     }
 }
