e8c7af8931519f36d37577691d2e2183ce5ede0c
VKVerma0210 <vkverma0210@gmail.com>
2021-08-09 15:12:55 +0530

    Predicate Example

--- Java8 Questions/PredicateExample.java
+++ Java8 Questions/PredicateExample.java
@@ -1,13 +1,26 @@
 import java.util.Arrays;
 import java.util.function.Predicate;
 
-public class PredicateAndExample {
+public class PredicateExample {
     public static void main(String[] args) {
         String arr[] = { "mumbai", "Delhi", "Pune", "Kolkata", "London", "Paris", "Pet" };
 
         Predicate<String> p1 = x -> x.length() > 3;
         Predicate<String> p2 = x -> x.startsWith("P");
 
+        // Predicate And
         Arrays.stream(arr).filter(p1.and(p2)).forEach(System.out::println);
+        System.out.println();
+
+        // Predicate Or
+        Arrays.stream(arr).filter(p1.or(p2)).forEach(System.out::println);
+        System.out.println();
+
+        // Predicate negate
+        for (String city : arr) {
+            if (p1.negate().test(city)) {
+                System.out.println(city);
+            }
+        }
     }
 }
